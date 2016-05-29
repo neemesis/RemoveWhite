34f854b69226f2e424f479668aa9955ac2131f66
Mirche Toshevski <mirche.toshevski@gmail.com>
2016-05-29 22:36:56 +0200

    first commit

---
+++ RemoveWhite/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace RemoveWhite {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblOpen = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.MaskedTextBox();
+            this.txt3 = new System.Windows.Forms.MaskedTextBox();
+            this.txt2 = new System.Windows.Forms.MaskedTextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.Title = "Open Image";
+            // 
+            // saveFile
+            // 
+            this.saveFile.Title = "Save Image";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(15, 25);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(169, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open Image";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblOpen
+            // 
+            this.lblOpen.AutoSize = true;
+            this.lblOpen.Location = new System.Drawing.Point(12, 9);
+            this.lblOpen.Name = "lblOpen";
+            this.lblOpen.Size = new System.Drawing.Size(0, 13);
+            this.lblOpen.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Remove pixels above these value:";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(15, 66);
+            this.txt1.Mask = "000";
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(49, 20);
+            this.txt1.TabIndex = 6;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(135, 66);
+            this.txt3.Mask = "000";
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(49, 20);
+            this.txt3.TabIndex = 7;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(75, 66);
+            this.txt2.Mask = "000";
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(49, 20);
+            this.txt2.TabIndex = 8;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(15, 92);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(169, 23);
+            this.btnProcess.TabIndex = 9;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(196, 124);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblOpen);
+            this.Controls.Add(this.btnOpen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Convert";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lblOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox txt1;
+        private System.Windows.Forms.MaskedTextBox txt3;
+        private System.Windows.Forms.MaskedTextBox txt2;
+        private System.Windows.Forms.Button btnProcess;
+    }
+}
+
---
+++ RemoveWhite/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoveWhite {
+    public partial class Form1 : Form {
+
+        Bitmap img;
+        bool imageOpen = false;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e) {
+            if (openFile.ShowDialog() == DialogResult.OK) {
+                img = (Bitmap) Image.FromStream(openFile.OpenFile());
+                lblOpen.Text = openFile.FileName.Substring(openFile.FileName.LastIndexOf('\\') + 1);
+                imageOpen = true;
+                Debug.WriteLine("Succ");
+                Debug.WriteLine(openFile.FileName);
+
+                txt1.Text = "200";
+                txt2.Text = "200";
+                txt3.Text = "200";
+            }
+            Debug.WriteLine(Color.Transparent);
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e) {
+            if (!Validator())
+                return;
+
+            int p1;
+            int.TryParse(txt1.Text, out p1);
+            int p2;
+            int.TryParse(txt2.Text, out p2);
+            int p3;
+            int.TryParse(txt3.Text, out p3);
+
+            for (int i = 0; i < img.Width; ++i) {
+                for (int j = 0; j < img.Height; ++j) {
+                    Color c = img.GetPixel(i, j);
+                    if (c.R > p1 && c.G > p2 && c.B > p3)
+                        img.SetPixel(i, j, Color.Transparent);
+                }
+            }
+            Debug.WriteLine("Done");
+
+            saveFile.Filter = "Images|*.png";
+            if (saveFile.ShowDialog() == DialogResult.OK) {
+                img.MakeTransparent(Color.White);
+                img.Save(saveFile.FileName);
+            }
+        }
+
+        private void txt_Validating(object sender, CancelEventArgs e) {
+            e.Cancel = Validator();
+        }
+
+        private bool Validator() {
+            int p1 = -1;
+            int.TryParse(txt1.Text, out p1);
+            int p2 = -1;
+            int.TryParse(txt2.Text, out p2);
+            int p3 = -1;
+            int.TryParse(txt3.Text, out p3);
+
+            if (p1 < 0 || p2 < 0 || p3 < 0 || !imageOpen) {
+                return false;
+            } else
+                return true;
+        }
+    }
+}
